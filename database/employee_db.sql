6eff9d0d3254aec10c70ef0e939a4cc9d3abe59e
jennermaxim <jennersi1remaxim@gmail.com>
2024-11-22 01:40:42 +0300

    Update the database

--- database/employee_db.sql
+++ database/employee_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 18, 2024 at 12:16 PM
+-- Generation Time: Nov 21, 2024 at 11:40 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,17 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `admin_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `contact` varchar(15) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `company` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_employees`
 --
 
 CREATE TABLE `tbl_employees` (
   `employee_id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `contact` varchar(15) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `full_name` varchar(100) NOT NULL,
+  `dob` date NOT NULL,
+  `nin` varchar(50) NOT NULL,
+  `e_email` varchar(100) NOT NULL,
+  `e_contact` varchar(15) NOT NULL,
+  `e_address` varchar(255) NOT NULL,
+  `job_title` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `supervisor` varchar(100) NOT NULL,
+  `employment_status` varchar(100) NOT NULL,
+  `start_date` date NOT NULL,
+  `employee_id_number` varchar(100) DEFAULT NULL,
+  `admin_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -42,20 +67,43 @@ CREATE TABLE `tbl_employees` (
 --
 
 --
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
 -- Indexes for table `tbl_employees`
 --
 ALTER TABLE `tbl_employees`
-  ADD PRIMARY KEY (`employee_id`);
+  ADD PRIMARY KEY (`employee_id`),
+  ADD KEY `fk_admin` (`admin_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tbl_employees`
 --
 ALTER TABLE `tbl_employees`
   MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_employees`
+--
+ALTER TABLE `tbl_employees`
+  ADD CONSTRAINT `fk_admin` FOREIGN KEY (`admin_id`) REFERENCES `tbl_admin` (`admin_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
